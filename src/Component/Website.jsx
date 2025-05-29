cbf7eb4b0fccd94a1c7d2a45dc84c2b017adc795
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2025-05-29 21:06:29 +0530

    Completed Pagenation

--- src/Component/Website.jsx
+++ src/Component/Website.jsx
@@ -1,58 +1,135 @@
 import React, { useEffect, useState } from "react";
-import CallIcon from "@mui/icons-material/Call";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
 import axios from "axios";
-import { Typography } from "@mui/material";
+import { Typography, Pagination, Card, CardMedia, CardContent, Grid, CircularProgress } from "@mui/material";
 
 const Website = () => {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
-   const [products,setProducts]=useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect (()=>{
-
-    const fetchData = async () =>{
-
-      try{
-        const res=await axios.get ("https://dummyjson.com/products");
-      
-        console.log(res.data.products);
-        setProducts(res.data.products)
-      }catch (error){
-        console.log('This Is A Error');
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("https://dummyjson.com/products");
+        setProducts(res.data.products);
+        setLoading(false);
+      } catch (error) {
+        console.error("This is an error:", error);
+        setLoading(false);
       }
     };
     fetchData();
+  }, []);
 
-  },[])
+  const handleChange = (event, value) => {
+    setCurrentPage(value);
+  };
 
+  // Calculate pagination
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(products.length / itemsPerPage);
 
   return (
-      <>
-    
-    <div>
-<Typography variant="h2">
-  Product Deatils
-</Typography>
+    <div style={{ padding: "2rem" }}>
+      <Typography variant="h4" gutterBottom>
+        Product Details
+      </Typography>
 
-  {products.map ((product)=>(
-  // <li  key={product.id} sx={{Color:'white'}}>{product.title}</li>
-  
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Grid container spacing={2}>
+            {currentItems.map((product) => (
+              <Grid item xs={12} sm={6} md={4} key={product.id}>
+                <Card>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={product.thumbnail}
+                    alt={product.title}
+                  />
+                  <CardContent>
+                    <Typography variant="h6">{product.title}</Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {product.description}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
 
- 
-
-
-   <img src={product.thumbnail}/>
-   ))}
-
-
-   
+          <Pagination
+            count={totalPages}
+            page={currentPage}
+            onChange={handleChange}
+            color="primary"
+            style={{ marginTop: "2rem", display: "flex", justifyContent: "center" }}
+          />
+        </>
+      )}
     </div>
-     </>
-  ); 
+  );
 };
 
 export default Website;
 
+// import React, { useEffect, useState } from "react";
+// import CallIcon from "@mui/icons-material/Call";
+// import LocationOnIcon from "@mui/icons-material/LocationOn";
+// import axios from "axios";
+// import { Typography } from "@mui/material";
+
+// const Website = () => {
+
+//    const [products,setProducts]=useState([]);
+
+//   useEffect (()=>{
+
+//     const fetchData = async () =>{
+
+//       try{
+//         const res=await axios.get ("https://dummyjson.com/products");
+      
+//         console.log(res.data.products);
+//         setProducts(res.data.products)
+//       }catch (error){
+//         console.log('This Is A Error');
+//       }
+//     };
+//     fetchData();
+
+//   },[])
+
+
+//   return (
+//       <>
+    
+//     <div>
+// <Typography variant="h2">
+//   Product Deatils
+// </Typography>
+
+//   {products.map ((product)=>(
+//   // <li  key={product.id} sx={{Color:'white'}}>{product.title}</li>
+
+
+//    <img src={product.thumbnail}/>
+//    ))}
+
+
+   
+//     </div>
+//      </>
+//   ); 
+// };
+
+// export default Website;
 
 
 
@@ -60,57 +137,3 @@ export default Website;
 
 
 
-
-// useEffect(() => {
-  //   // ?Get Method
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get("https://dummyjson.com/recipes");
-  //       setData(response.data.recipes);
-  //     } catch (error) {
-  //       console.log("Error fetching:", error);
-  //     }
-  //   };
-    // <div sx={{backgroundcolor:white}}>
-    //   <>
-    //     {/* <img style={{width:"100rem", height:"30%",}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc8SKpaoBT9EWK5cjkiXwD5BqMmHEVqwGnysRb5n8nr1dNDh4ANNV1uP4rjGjjakloPnQ&usqp=CAU"    alt="" /> */}
-    //     <Grid item xs={2}>
-    //       <Grid>
-    //         <Box sx={{ paddingTop: "12rem", paddingLeft: "14rem" }}>
-    //           <Typography variant="h2" sx={{ fontSize: "3rem" }}>
-    //             Get in Touch with <br /> TCP International 👋
-    //           </Typography>
-
-    //           <Typography
-    //             variant="body1"
-    //             sx={{ marginTop: "1rem", maxWidth: "600px" }}
-    //           >
-    //             Feel free to connect with us for any of your needs regarding our{" "}
-    //             <br />
-    //             services. Our support team is ready to solve any of your issues.{" "}
-    //             <br />
-    //             Just push a text to us and we will get back to you immediately.
-    //           </Typography>
-    //         </Box>
-    //       </Grid>
-    //     </Grid>
-    //     <Grid>
-    //       <Box>
-    //         <Typography>Drop Us a Message</Typography>
-    //       </Box>
-    //     </Grid>
-
-    //     <Typography
-    //       variant="body1"
-    //       sx={{ marginLeft: "14rem", marginTop: "1rem", fontSize: "1.3rem" }}
-    //     >
-    //       <LocationOnIcon sx={{ marginTop: "2" }} /> Bhubaneswar, Odisha 751024
-    //       <br />
-    //       <MailIcon sx={{ paddingTop: "2" }} />
-    //       info@TCP.com
-    //       <br />
-    //       <CallIcon sx={{ paddingTop: "2" }} /> +91 7008166042
-    //       <br />
-    //     </Typography>
-    //   </>
-    // </div>
